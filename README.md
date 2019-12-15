0cf751c8029a61f42e479a832dc2aae694947ff9
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2019-12-15 15:32:49 -0300

    Termina o primeiro audio

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Filtros Digitais
+
+# Referencias
+
+https://dsp.stackexchange.com/questions/49460/apply-low-pass-butterworth-filter-in-python
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import scipy.io
+import scipy.io.wavfile as wav
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fftpack import fft
@@ -11,21 +11,25 @@ audioBruto = scipy.io.loadmat('proj.mat')
 audioBruto = audioBruto.get('r')[0]
 
 fs = 44100  # Frequência de amostragem
-fc = 12000  # Frequência de corte
+fc = 11500  # Frequência de corte
 w = fc / (fs / 2)  # Normalização
 
 # Cria o filtro
-b, a = scipy.signal.butter(10, w, 'low')
+b, a = scipy.signal.butter(20, w, 'low')
 
 # Executa o Filtro no sinal
 output = scipy.signal.filtfilt(b, a, audioBruto)
 
+sinal2 = scipy.signal.hilbert(output)
+
 # Faz a transformada de fourier
-frequencia = np.fft.rfft(output)
+frequencia = np.fft.rfft(np.abs(sinal2))
+
+wav.write('saida.wav', 44100, abs(sinal2))
 
 # # Gráfico da Mensagem
 # plt.title('Mensagem')
-plt.plot(abs(frequencia))
+plt.plot(abs(sinal2))
 # plt.xlabel('Tempo')
 # plt.ylabel('Amplitude')
 plt.grid(True)
